fb54052fc1f7a52fa395878cbf9396c487da36b1
y4v8 <y4v8@yandex.ua>
2019-01-10 11:49:41 +0200

    выбор целевой платформы в файле проекта

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Platform=x86 /p:Configuration=Release src\rtext.x86.csproj
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Platform=x64 /p:Configuration=Release src\rtext.x64.csproj
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:PlatformTarget=x86 src\rtext.csproj
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:PlatformTarget=x64 src\rtext.csproj
